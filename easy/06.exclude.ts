8e365193e1599bf2ecb9f0bc4ac1a4895cbe7f4b
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-01-09 23:49:41 -0400

    adding easy challenge: 06.exclude

---
+++ easy/06.exclude.ts
@@ -0,0 +1,9 @@
+// Implement the built-in Exclude<T, U>
+// Exclude from T those types that are assignable to U
+// For example:
+
+type Result = MyExclude<"a" | "b" | "c", "a">; // 'b' | 'c'
+
+type MyExclude<T extends string, K extends T> = T extends K ? never : T;
+
+export {};

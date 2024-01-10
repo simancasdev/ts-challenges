00560d4d784b71cc2d0e250104be752cce05e978
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-01-09 23:37:11 -0400

    adding easy challenge: 03-tuple-to-object

---
+++ easy/03.tuple-to-object.ts
@@ -0,0 +1,13 @@
+// Given an array, transform it into an object type and the key/value must be in the provided array.
+// For example:
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type result = TupleToObject<typeof tuple>; // expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
+// --------------Solution--------------
+type TupleToObject<L extends readonly string[]> = {
+  [K in L[number]]: K;
+};
+
+export {};

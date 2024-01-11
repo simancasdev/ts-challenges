43cda2ca91f7288da214fa28cefcfda6106fb7b5
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-01-11 00:41:13 -0400

    removing variable

--- easy/13.parameters.ts
+++ easy/13.parameters.ts
@@ -2,8 +2,6 @@
 // Implement the built-in Parameters generic without using it.
 // For example:
 
-const hello = "string";
-
 const foo = (arg1: string, arg2: number): void => {};
 
 type FunctionParamsType = MyParameters<typeof foo>; // [arg1: string, arg2: number]

7c81c56077873c4b0759e7a9430a40cc4295b1ea
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-01-11 00:12:44 -0400

    adding easy challenge: 10.includes

---
+++ easy/10.includes.ts
@@ -0,0 +1,9 @@
+// Includes
+// Implement the JavaScript Array.includes function in the type system.
+// A type takes the two arguments. The output should be a boolean true or false.
+
+type isPillarMen = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Santana">; // expected to be `false`
+
+type Includes<T extends string[], K extends string> = K extends T[number]
+  ? true
+  : false;

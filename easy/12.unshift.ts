3023240e27fd5be2673343f6d86541081cf02a4d
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-01-11 00:20:35 -0400

    adding easy challenge: 12.unshift

---
+++ easy/12.unshift.ts
@@ -0,0 +1,7 @@
+// Unshift
+// Implement the type version of Array.unshift
+// For example:
+
+type Result = Unshift<[1, 2], 0>; // [0, 1, 2]
+
+type Unshift<T extends number[], K extends number> = [K, ...T];

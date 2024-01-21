e2d10fcd16c75797a9b4f8ff2cc561182fdbe20b
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-01-21 13:02:40 -0400

    adding medium challenge: 23.kebab-case

---
+++ medium/23.kebab-case.ts
@@ -0,0 +1,19 @@
+// KebabCase
+// Replace the camelCase or PascalCase string with kebab-case.
+// FooBarBaz -> foo-bar-baz
+// For example
+
+type FooBarBaz = KebabCase<"FooBarBaz">;
+const foobarbaz: FooBarBaz = "foo-bar-baz";
+
+type DoNothing = KebabCase<"do-nothing">;
+const doNothing: DoNothing = "do-nothing";
+
+// --------------Solution--------------
+type KebabCase<T extends string> = T extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${KebabCase<S2>}`
+    : `${Uncapitalize<S1>}-${KebabCase<S2>}`
+  : T;
+
+export {};
